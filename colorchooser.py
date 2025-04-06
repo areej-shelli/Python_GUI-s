6fcdb656d9612069166b276265f1c5540f0c7001
areej-shelli <124439498+areej-shelli@users.noreply.github.com>
2025-04-06 23:47:12 +0300

    Delete colorchooser.py

--- colorchooser.py
+++
@@ -1,9 +0,0 @@
-from tkinter import *
-from tkinter import colorchooser
-root=Tk()
-def show():
-    myc=colorchooser.askcolor()[1]
-    l=Label(root,text=myc).pack()
-    l=Label(root,text="New color",bg=myc).pack()
-b=Button(root,text="Colors",command=show).pack()
-root.mainloop()
